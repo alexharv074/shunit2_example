94eac18531d113e09a2705ab284d4f20e4d0aa79
Alex Harvey <alexharv074@gmail.com>
2018-09-08 17:13:20 +1000

    Add second example to test bad inputs

--- shunit2/delete_stack.sh
+++ shunit2/delete_stack.sh
@@ -5,13 +5,22 @@ script_under_test=$(basename $0)
 aws() {
   echo "aws $*" >> commands_log
   case "aws $*" in
-    'aws ec2 delete-key-pair --key-name mystack') true ;;
-    'aws s3 rm --recursive s3://mybucket/deployments/mystack') true ;;
-    'aws cloudformation describe-stack-resources --stack-name mystack --query StackResources[?ResourceType==`AWS::AutoScaling::AutoScalingGroup`].PhysicalResourceId --output text')
+    "aws ec2 delete-key-pair --key-name mystack") true ;;
+    "aws s3 rm --recursive s3://mybucket/deployments/mystack") true ;;
+
+    "aws cloudformation describe-stack-resources \
+--stack-name mystack \
+--query "'StackResources[?ResourceType==`AWS::AutoScaling::AutoScalingGroup`].PhysicalResourceId'" \
+--output text")
       echo mystack-AutoScalingGroup-xxxxxxxx
       ;;
-    'aws autoscaling resume-processes --auto-scaling-group-name mystack-AutoScalingGroup-xxxxxxxx') true ;;
-    'aws cloudformation delete-stack --stack-name mystack') true ;;
+
+    "aws autoscaling resume-processes \
+--auto-scaling-group-name mystack-AutoScalingGroup-xxxxxxxx")
+      true
+      ;;
+
+    "aws cloudformation delete-stack --stack-name mystack") true ;;
     *) echo "No response for >>> aws $*" ;;
   esac
 }
@@ -34,6 +43,12 @@ EOF
 
   assertEquals "unexpected sequence of commands issued" \
     "" "$(diff -wu expected_log commands_log)"
+}
+
+testBadInputs() {
+  actual_stdout=$(. $script_under_test too many arguments passed in)
+  assertTrue "unexpected response when passing bad inputs" \
+    "echo $actual_stdout | grep -q ^Usage"
 }
 
 . shunit2
